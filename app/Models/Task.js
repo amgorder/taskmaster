f9df6201756c9b65c7f750cc7f4c4d031d7b8369
Aaron Gorder <aarongorder@gmail.com>
2021-02-23 09:31:31 -0700

    stuff

--- app/Models/Task.js
+++ app/Models/Task.js
@@ -18,20 +18,24 @@ export default class Task {
         <h1>${this.title}<button class="text-danger close mt-2"
                 onclick="app.taskController.delete('${this.id}')"><span>&times;</span></button></h1>
         <h5>New To Do</h5>
-        <p>Choose your Task's colors:</p>
+        <p>Choose your Todo's colors:</p>
 
-<div>
-    <input type="color" id="head" name="head"
-           value="#e66465">
-</div>
-
-
-        <form onsubmit="app.todosController.create(event, '${this.id}')">
-            <div class="form-group">
-                <input  type="text" name="title" minlength="3char" maxlength="50char" placeholder="Enter To Do Here...">
-                <button class="btn btn-success btn-sm" type="submit"> <i class="fa fa-plus" aria-hidden="true"></i></button>
+        
+        
+        <form onsubmit="app.todosController.create(event, '${this.id}',)">
+        <div class="form-group">
+        
+        <div>
+            <input type="color" id="head" name="color" value="#e66465">
+        </div>
+                <input type="text" name="title" minlength="3char" maxlength="50char"
+                    placeholder="Enter To Do Here...">
+                <button class="btn btn-success btn-sm" type="submit"> <i class="fa fa-plus"
+                        aria-hidden="true"></i></button>
                 <div class="row">
-                    ${this.Todo}
+                    <div class="col">
+                        <h4><span>${this.Todo}</span></h4>
+                    </div>
                 </div>
             </div>
         </form>
--- app/Models/Todo.js
+++ app/Models/Todo.js
@@ -2,25 +2,26 @@ import { generateId } from "../Utils/GenerateId.js";
 
 export default class ToDo {
 
-    constructor({ title, id = generateId(), taskId }) {
+    constructor({ title, id = generateId(), taskId, color }) {
 
         this.title = title
         this.id = id
         this.taskId = taskId
+        this.color = color
     }
 
     get Template() {
         console.log("todo template called");
         return /* html */ `
-                <div class="col">
-                <card>
-                    <h1 class="${this.title.includes('now') ? 'text-danger' : ''}">${this.title} <button type="button"
-                    class="text-danger close mt-3" onclick="${this.title.strike}"> <span><i class="fa fa-check fa-color-success" aria-hidden="true"></i></span> </button> <button type="button"
-                            class="text-danger close mt-3" onclick="app.todosController.delete('${this.id}')">
-                            <span>&times;</span> </button> </h1>
-                </card>
-                </div>
+        <div class="col">
 
+        <h1  class="${this.title.includes('now') ? 'text-danger' : ''}">${this.title} <button type="button"
+                class="text-success close mt-3" onclick="app.todosController.lineThrough('${this.id}')">
+                <span><i class="fa fa-check fa-color-success" aria-hidden="true"></i></span> </button> <button
+                type="button" class="text-danger close mt-3" onclick="app.todosController.delete('${this.id}')">
+                <span>&times;</span> </button> </h1>
+
+    </div>
         `
 
     }
